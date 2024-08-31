8a2a3dc82bb51f4138a6168dfb049d9b24933bec
Adam M <144551653+BlondieBumble1@users.noreply.github.com>
2024-08-31 20:40:04 +0100

    Completed Problem 10 Summation Of Primes

---
+++ python/Problem 10 Summation Of Primes.py
@@ -0,0 +1,19 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# Find the sum of all the primes below two million.
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+primes = [i for i in range(2, 2_000_000) if is_prime(i)]
+print(sum(primes))
